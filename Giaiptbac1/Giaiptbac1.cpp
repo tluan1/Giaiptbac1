aba9f828098534e66e2496f6aada1f20ccaef71d
ADMIN <ADMIN@LAB1-MAY14>
2024-02-19 16:33:22 +0700

    Add project files.

---
+++ Giaiptbac1/Giaiptbac1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a, b;
+	float x;
+	cout << "UNG DUNG GIAI PHUONG TRINH BAC I\n";
+	cout << "Nhap a: "; cin >> a;
+	cout << "Nhap b: "; cin >> b;
+	if (a == 0) 
+	{ 
+		if (b == 0)
+		{
+			cout << "PT dung voi moi x";
+		}
+		else {
+			cout << "PT vo nghiem\n";
+		}
+	}
+	else {
+		x = (float)-b / a;
+		cout << "ket qua x=" << x << endl;
+	}
+	return 0;
